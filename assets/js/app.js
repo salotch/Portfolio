73bd4bd2578a52bf3384f25f4d9713341ba8e742
DELL <salma.a.samei@gmail.com>
2024-11-11 09:15:25 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+const sections = document.querySelectorAll(".section");
+const secBtns = document.querySelectorAll(".controls");
+const secBtn = document.querySelectorAll(".control");
+const allSections = document.querySelectorAll(".main-content");
+
+function pageTranstions() {
+  //button click active class
+  for (let i = 0; i < secBtn.length; i++) {
+    secBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += " active-btn";
+    });
+  }
+  //sections active class
+  allSections[0].addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+
+    if (id) {
+      //remove selected ele from the other btns
+      secBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      secBtn.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
+  // toggle theme
+
+  const papaSwitcher = document.getElementById("theme-btn");
+  const switcher = document.getElementById("ball-switcher");
+
+  papaSwitcher.addEventListener("click", () => {
+     let element = document.body;
+     if (switcher) {
+      if (papaSwitcher.style.justifyContent == "flex-start") {
+       
+        element.classList.toggle("light-mode");
+        papaSwitcher.style.justifyContent = "flex-end";
+      } else {
+        element.classList.toggle("light-mode");
+        papaSwitcher.style.justifyContent = "flex-start";
+      }
+    }
+  });
+ 
+  
+}
+
+pageTranstions();
---
+++ assets/js/dist/app.dev.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var sections = document.querySelectorAll(".section");
+var secBtns = document.querySelectorAll(".controls");
+var secBtn = document.querySelectorAll(".control");
+var allSections = document.querySelectorAll(".main-content");
+
+function pageTranstions() {
+  //button click active class
+  for (var i = 0; i < secBtn.length; i++) {
+    secBtn[i].addEventListener("click", function () {
+      var currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace("active-btn", "");
+      this.className += " active-btn";
+    });
+  } //sections active class
+
+
+  allSections[0].addEventListener("click", function (e) {
+    var id = e.target.dataset.id;
+
+    if (id) {
+      //remove selected ele from the other btns
+      secBtns.forEach(function (btn) {
+        btn.classList.remove("active");
+      });
+      secBtn.forEach(function (btn) {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active"); //hide other sections
+
+      sections.forEach(function (section) {
+        section.classList.remove("active");
+      });
+      var element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  }); // toggle theme
+
+  var papaSwitcher = document.getElementById("theme-btn");
+  var switcher = document.getElementById("ball-switcher");
+  papaSwitcher.addEventListener("click", function () {
+    var element = document.body;
+
+    if (switcher) {
+      if (papaSwitcher.style.justifyContent == "flex-start") {
+        element.classList.toggle("light-mode");
+        papaSwitcher.style.justifyContent = "flex-end";
+      } else {
+        element.classList.toggle("light-mode");
+        papaSwitcher.style.justifyContent = "flex-start";
+      }
+    }
+  });
+}
+
+pageTranstions();
